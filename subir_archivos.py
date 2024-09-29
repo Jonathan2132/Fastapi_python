e5800a24a83192095069ac922037f9d5aaa0f68c
jonathan <jbcll488@gmail.com>
2024-09-28 21:03:30 -0400

    h

---
+++ subir_archivos.py
@@ -0,0 +1,26 @@
+import shutil
+import os
+from fastapi import FastAPI, UploadFile,File
+
+app = FastAPI()
+
+@app.post("/subir")
+async def subir(file: UploadFile = File(...)):
+    saze = len(await file.read()) / (1024*1024)
+    name_archivo = file.filename
+    type_archivo = file.content_type
+    
+
+    locacion = f"archivos/{name_archivo}"
+
+    with open(locacion,"wb")  as abierto:
+        shutil.copyfileobj(file.file,abierto)
+
+
+    return{"name":name_archivo,"saze":saze,"type":type_archivo}
+
+
+
+
+
+
